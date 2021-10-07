113b14f15c34920ec383c80232b267279604929b
Jordan Riesel <jwald3@brockport.edu>
2021-10-07 10:14:46 -0400

    Turned switch statement into a function. Pending fix to error handling (to avoid invalid input)

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 
+double calculate(double num1, double num2, char op) {
+    // handle switch statement in its own function for clarity in main function
+    switch(op)
+    {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        case '/':
+            return num1 / num2;
+        default:
+            std::cout << "Invalid input!\n";
+    }
+}
+
 int main()
 {
     double num1{ };
@@ -15,25 +32,7 @@ int main()
     std::cout << "Enter second number: ";
     std::cin >> num2;
 
-    double result{ };
-
-    switch(op)
-    {
-        case '+':
-            result = num1 + num2;
-            break;
-        case '-':
-            result = num1 - num2;
-            break;
-        case '*':
-            result = num1 * num2;
-            break;
-        case '/':
-            result = num1 / num2;
-            break;
-        default:
-            std::cout << "Invalid input!\n";
-    }
+    double result{calculate(num1, num2, op)};
 
     std::cout << result;
 }
