1de769806132280fec41994df83b4d3341eaf7fd
Jordan Riesel <jwald3@brockport.edu>
2021-10-07 11:47:45 -0400

    Added program state variables and while loops to enable program to be used without rerunning for each equation.

--- main.cpp
+++ main.cpp
@@ -22,17 +22,36 @@ int main()
     double num1{ };
     double num2{ };
     char op{}; // "operator" is a reserved keyword, otherwise I would use that identifier
+    bool calcOn{ true }; // will handle program state, while true, user can calculate again
+    char contCalc{ };
 
-    std::cout << "Enter first number: ";
-    std::cin >> num1;
+    while (calcOn) {
+        // calculator will run until user ends it
+        std::cout << "Enter first number: ";
+        std::cin >> num1;
 
-    std::cout << "Enter operator: ";
-    std::cin >> op;
+        std::cout << "Enter operator: ";
+        std::cin >> op;
 
-    std::cout << "Enter second number: ";
-    std::cin >> num2;
+        std::cout << "Enter second number: ";
+        std::cin >> num2;
 
-    double result{calculate(num1, num2, op)};
+        double result{calculate(num1, num2, op)};
 
-    std::cout << result;
+        std::cout << result << '\n';
+        contCalc = '\0'; // reset (otherwise program won't let you change the option later)
+
+        while (contCalc != 'n' && contCalc != 'y') {
+            // handles incorrect input, keeps asking until y or n is entered
+            std::cout << "Calculate again? (y for yes, n for no)";
+            std::cin >> contCalc;
+        }
+
+        if (contCalc == 'n') {
+            // end program by toggling
+            calcOn = false;
+        }
+    }
+
+    std::cout << "Have a nice day!";
 }
